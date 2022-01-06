ee69236015664c43966a6006cbd876ed57c6c178
Sodaman <mattrick2214@gmail.com>
2022-01-06 15:45:09 -0600

    done adding questions

--- Develop/index.js
+++ Develop/index.js
@@ -37,6 +37,83 @@ const promptQuestions = () => {
                     return false;
                 }
             }
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Please explain what your project is about.',
+            validate: titleInput => {
+                if (titleInput) {
+                    return true;
+                } else{
+                    console.log('Please enter a title name for your Readme!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'confirm',
+            name: 'confirmContents',
+            message: 'Would you like to include a table of contents?',
+            default: true
+        },
+        {
+            type: 'checkbox',
+            name: 'contents',
+            message: 'What sections of this page would you like to include in your Readme? (Check which you want)',
+            choices: ['Description', 'Contents', 'Installation', 'Usage', 'License', 'Contribution', 'Tests', 'Questions'],
+            when: ({ confirmContents }) => {
+                if (confirmContents) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'installation',
+            message: 'How would one sitting at home be able to use your application?',
+        },
+        {
+            type: 'input',
+            name: 'usage',
+            message: 'What are the uses for this application?',
+        },
+        {
+            type: 'input',
+            name: 'contribution',
+            message: 'Would you like to list any fellow collaborators in this project?',
+        },
+        {
+            type: 'input',
+            name: 'license',
+            message: 'Would you like to include a license?',
+        },
+        {
+            type: 'input',
+            name: 'badges',
+            message: 'Would you like to include a badge at the top of your ReadME?',
+        },
+        {
+            type: 'input',
+            name: 'features',
+            message: 'What other features does your product contain?',
+        },
+        {
+            type: 'input',
+            name: 'tests',
+            message: 'Would you like to include tests?',
+        },
+        {
+            type: 'input',
+            name: 'questions',
+            message: 'Would you like to add any additional questions?',
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: 'Would you like to include your github username?'
         }
     ])
 }
@@ -68,64 +145,3 @@ promptQuestions()
 
 
 
-
-    //     {
-    //         type: 'input',
-    //         name: 'description',
-    //         message: 'Please explain what your project is about.',
-    //         validate: titleInput => {
-    //             if (titleInput) {
-    //                 return true;
-    //             } else{
-    //                 console.log('Please enter a title name for your Readme!');
-    //                 return false;
-    //             }
-    //         }
-    //     },
-    //     {
-    //         type: 'input',
-    //         name: 'contents',
-    //         message: 'Would you like to include a table of contents?'
-    //     },
-    //     {
-    //         type: 'input',
-    //         name: 'installation',
-    //         message: 'How would one sitting at home be able to use your application?',
-    //         default: false
-    //     },
-    //     {
-    //         type: 'input',
-    //         name: 'usage',
-    //         message: 'What are the uses for this application?',
-    //         default: false
-    //     },
-    //     {
-    //         type: 'input',
-    //         name: 'contribution',
-    //         message: 'Would you like to list any fellow collaborators in this project?',
-    //         default: false
-    //     },
-    //     {
-    //         type: 'input',
-    //         name: 'licesnse',
-    //         message: 'Would you like to include a license?',
-    //         default: false
-    //     },
-    //     {
-    //         type: 'input',
-    //         name: 'badge',
-    //         message: 'Would you like to include a badge at the top of your ReadME?',
-    //         default: false
-    //     },
-    //     {
-    //         type: 'input',
-    //         name: 'features',
-    //         message: 'What other features does your product contain?',
-    //         default: false
-    //     },
-    //     {
-    //         type: 'input',
-    //         name: 'tests',
-    //         message: 'Would you like to include tests?',
-    //         default: false
-    //     },
