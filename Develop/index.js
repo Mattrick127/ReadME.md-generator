345f4c72cea36b758e540c117011e1253efe4005
Sodaman <mattrick2214@gmail.com>
2022-01-06 12:30:59 -0600

    first commit, got back a question

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,16 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const generateReadMe = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
 const questions = [];
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is the title of your project!?'
+        }
+    ])
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
@@ -11,3 +20,4 @@ function init() {}
 
 // Function call to initialize app
 init();
+// "title", "description", "tableOfContents", "Installation", "Usage", "License", "Contributing", "Tests", "Questions"
