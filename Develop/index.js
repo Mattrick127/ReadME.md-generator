1ec8a402cf02c2c8f5dd649d21bfc50293e72801
Sodaman <mattrick2214@gmail.com>
2022-01-06 13:12:16 -0600

    served up a few questions for testing

--- Develop/index.js
+++ Develop/index.js
@@ -8,9 +8,77 @@ const questions = [];
         {
             type: 'input',
             name: 'name',
-            message: 'What is the title of your project!?'
-        }
-    ])
+            message: 'What is the title of your project!?',
+            validate: titleInput => {
+                if (titleInput) {
+                    return true;
+                } else{
+                    console.log('Please enter a title name for your Readme!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'name',
+            message: 'Please explain what your project is about.',
+            validate: titleInput => {
+                if (titleInput) {
+                    return true;
+                } else{
+                    console.log('Please enter a title name for your Readme!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'name',
+            message: 'Would you like to include a table of contents?'
+        },
+        {
+            type: 'input',
+            name: 'name',
+            message: 'How would one sitting at home be able to use your application?',
+            default: false
+        },
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What are the uses for this application?',
+            default: false
+        },
+        {
+            type: 'input',
+            name: 'name',
+            message: 'Would you like to list any fellow collaborators in this project?',
+            default: false
+        },
+        {
+            type: 'input',
+            name: 'name',
+            message: 'Would you like to include a license?',
+            default: false
+        },
+        {
+            type: 'input',
+            name: 'name',
+            message: 'Would you like to include a badge at the top of your ReadME?',
+            default: false
+        },
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What other features does your product contain?',
+            default: false
+        },
+        {
+            type: 'input',
+            name: 'name',
+            message: 'Would you like to include tests?',
+            default: false
+        },
+    ]);
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
@@ -20,4 +88,3 @@ function init() {}
 
 // Function call to initialize app
 init();
-// "title", "description", "tableOfContents", "Installation", "Usage", "License", "Contributing", "Tests", "Questions"
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -14,7 +14,7 @@ function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `# ${data.titleInput}
 
 `;
 }
